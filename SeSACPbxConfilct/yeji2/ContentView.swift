67447a19e8c818e8e24eeb51fdfc7820a77cc617
Yeji-Jang1210 <jyg8727@gmail.com>
2024-10-08 11:35:44 +0900

    yeji2 작업 완료

--- SeSACPbxConfilct/yeji2/ContentView.swift
+++ SeSACPbxConfilct/yeji2/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "popcorn")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, yeji2!")
         }
         .padding()
     }
